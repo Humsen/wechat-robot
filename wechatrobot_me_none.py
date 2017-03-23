a8249d08e3b16438b25982bcc06343f6889a9c41
HelloHusen <940706904@qq.com>
2017-03-23 15:54:15 +0800

    增加远程管理员功能，远程控制机器人启动和休眠

--- wechatrobot_me_none.py
+++ wechatrobot_me_none.py
@@ -1,43 +1,78 @@
 ﻿# coding: utf-8
 
 import os
+import threading
 import requests
 import time
 from wxpy import *
 import json
 
+#微信机器人
+robot=Bot()
+
 #图灵机器人
 tuling = Tuling(api_key='0d26f7c76ecf4623a536368eaf3d26ea')
 
-#图灵机器人
+#图灵机器人手动消息
 def talks_robot(info = 'Husen'):
     api_url = 'http://www.tuling123.com/openapi/api'
     apikey = '0d26f7c76ecf4623a536368eaf3d26ea'
-    data = {'key': apikey,
-                'info': info}
+    data = {'key': apikey,'info': info}
     req = requests.post(api_url, data=data).text
     replys = json.loads(req)['text']
     return replys
 
-#微信自动回复
-#robot = Robot()
-robot=Bot(True,1)
+# 定义远程管理员 (用于远程管理)，使用备注名更安全
+remote_admin = ensure_one(robot.friends().search(remark_name='Husen'))
 
 #mps = robot.mps(update=True)
 group_1 = robot.groups().search('来呀。来呀。')[0]
 #group_2 = robot.groups().search('只是爱要怎么说 出口')[0]
 group_3 = robot.groups().search('421永不断片')[0]
 
+#动态关闭除启动函数之外的注册函数
+def remote_down():
+    robot.registered.disable()
+    robot.registered.enable(remote_up)
+
+#开启所有注册函数
+def remote_reup():
+    robot.registered.enable()
+
+# 远程启动函数
+@robot.register()
+def remote_up(msg):
+    if (msg.is_at and msg.member == remote_admin and '启动' in msg.text):
+    #if ('启动' in msg.text):
+         thread = threading.Thread(target=remote_reup)
+         thread.start()
+         thread.join()
+         return '已启动'
+    else:
+         return
+
 # 回复来自其他好友、群聊和公众号的消息
 @robot.register([Friend])
 def reply_my_friend(msg):
-    tuling.do_reply(msg)
+    if (msg.is_at and msg.member == remote_admin and '休眠' in msg.text):
+        thread = threading.Thread(target=remote_down)
+        thread.start()
+        thread.join()
+        return '已休眠'
+    else:
+        tuling.do_reply(msg)
 
 # 如果是群聊，但没有被 @，则不回复
 @robot.register([Group])
 def auto_reply(msg):
     if not (isinstance(msg.sender, Group) and not msg.is_at):
-        tuling.do_reply(msg)
+        if (msg.is_at and msg.member == remote_admin and '休眠' in msg.text):
+            thread = threading.Thread(target=remote_down)
+            thread.start()
+            thread.join()
+            return '已休眠'
+        else:
+            tuling.do_reply(msg)
 
  # 如果是群聊，而且是语音，但没有被 @，则不回复
 @robot.register([Group],RECORDING)
@@ -66,17 +101,23 @@ def ignore(msg):
 """
 
 #接收一些群
-@robot.register([group_1,group_3])
+@robot.register([group_1,group_3,remote_admin])
 def ignore(msg):
-    tuling.do_reply(msg)
+    #if ('休眠' in msg.text):
+    if (msg.is_at and msg.member == remote_admin and '休眠' in msg.text):
+        thread = threading.Thread(target=remote_down)
+        thread.start()
+        thread.join()
+        return '已休眠'
+    else:
+        tuling.do_reply(msg)
 
 @robot.register(msg_types=FRIENDS)
 def auto_accept_friends(msg):
-    if 'wxpy' in msg.text.lower():
-        new_friend = robot.accept_friend(msg.card)
-        # 或 new_friend = msg.card.accept()
-        # 向新的好友发送消息
-        new_friend.send('哈哈，我自动接受了你的好友请求')
+    new_friend = robot.accept_friend(msg.card)
+    # 或 new_friend = msg.card.accept()
+    # 向新的好友发送消息
+    new_friend.send('哈哈，我自动接受了你的好友请求')
 
 groups = robot.groups(True)
 #整点报时
@@ -87,16 +128,16 @@ while 1:
 
     if (hour == '07' and minutes == '30' and seconds == '00'):
         nowTime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-        group_1.send('早上好！为您整点报时：\n{}\n--今日天气--\n{}'.format(nowTime,talks_robot(info='重庆沙坪坝区天气')))
-        group_3.send('早上好！为您整点报时：\n{}\n--今日天气--\n{}'.format(nowTime,talks_robot(info='南京江宁区天气')))
+        group_1.send('早上好！\n起床啦，为您整点报时：\n{}\n-----今日天气-----\n{}'.format(nowTime,talks_robot(info='重庆沙坪坝区天气')))
+        group_3.send('早上好！\n起床啦，为您整点报时：\n{}\n-----今日天气-----\n{}'.format(nowTime,talks_robot(info='南京江宁区天气')))
     elif(hour == '13' and minutes == '30' and seconds == '00'):
         nowTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         for group in groups:
-            group.send('下午好！该干活了，为您整点报时：\n{}\n--轻松一下--\n{}'.format(nowTime,talks_robot(info='讲个笑话')))
+            group.send('下午好！\n该干活了，为您整点报时：\n{}\n-----轻松一下-----\n{}'.format(nowTime,talks_robot(info='讲个笑话')))
     elif (hour == '00' and minutes == '00' and seconds == '00'):
-            nowTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-            for group in groups:
-                group.send('晚上好！该吃宵夜了，为您整点报时：\n{}\n--晚安全世界--'.format(nowTime))
+        nowTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        for group in groups:
+            group.send('晚上好！\nIt\'s 宵夜time，为您整点报时：\n{}\n-----晚安全世界-----'.format(nowTime))
 
     time.sleep(1)
 
